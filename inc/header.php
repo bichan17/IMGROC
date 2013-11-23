db749f2c0dcda49cabe7a4cd0c5095f0f3473e81
Vlad Ionescu <vlad@Vlads-MacBook-Pro.local>
2013-11-23 14:25:35 -0500

    Fixed asset link for local dev server

--- inc/header.php
+++ inc/header.php
@@ -9,8 +9,8 @@
 		?>
 		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=2">
-		<link rel="stylesheet" type="text/css" media="screen" href="../imgroc/style/style.css" />
-		<script type="text/javascript" src="../imgroc/scripts/modernizr.js"></script>
+		<link rel="stylesheet" type="text/css" media="screen" href="/style/style.css" />
+		<script type="text/javascript" src="/scripts/modernizr.js"></script>
 		<?php
 			if (isset($customScripts))
 				for ($i = 0; $i < count($customScripts); $i++)
